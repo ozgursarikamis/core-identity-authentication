640cd9ccf8f153344b4f3521e132658b133d1a08
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-24 00:08:00 +0300

    claims, identity, providers

--- AuthorizeApi/Controllers/HomeController.cs
+++ AuthorizeApi/Controllers/HomeController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,7 +12,6 @@ namespace AuthorizeApi.Controllers
         {
             return View();
         }
-        
         [Authorize]
         public IActionResult Secret()
         {
@@ -18,6 +20,27 @@ namespace AuthorizeApi.Controllers
 
         public IActionResult Authenticate()
         {
+            var grandmaClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Email, "bob@fmail.com"),
+                new Claim("Grandma.Says", "very nice boy"),
+            };
+            
+            var licenseClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, "bob@gmail.com"),
+                new Claim("DrivingLicense", "A+")
+            };
+            
+            
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government Identity");
+            
+            var userPrincipal = new ClaimsPrincipal(new []{ grandmaIdentity, licenseIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+            // return View();
             return RedirectToAction("Index");
         }
     }
