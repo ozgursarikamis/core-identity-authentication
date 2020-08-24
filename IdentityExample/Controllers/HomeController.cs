ff8f241af1f453514c111cd70176dc7c45ad30f0
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-24 14:35:48 +0300

    Email Verification

--- IdentityExample/Controllers/HomeController.cs
+++ IdentityExample/Controllers/HomeController.cs
@@ -5,101 +5,132 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NETCore.MailKit.Core;
 
 namespace IdentityExample.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signinManager;
+	public class HomeController : Controller
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly SignInManager<IdentityUser> _signinManager;
+		private readonly IEmailService _emailService;
 
-        public HomeController(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager
-        )
-        {
-            _userManager = userManager;
-            _signinManager = signInManager;
-        }
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public HomeController(
+			UserManager<IdentityUser> userManager,
+			SignInManager<IdentityUser> signInManager,
+			IEmailService emailService
+		)
+		{
+			_userManager = userManager;
+			_signinManager = signInManager;
+			_emailService = emailService;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        [Authorize]
-        public IActionResult Secret()
-        {
-            return View();
-        }
+		[Authorize]
+		public IActionResult Secret()
+		{
+			return View();
+		}
 
-        public IActionResult Authenticate()
-        {
-            var grandmaClaims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, "Bob"),
-                new Claim(ClaimTypes.Email, "bob@fmail.com"),
-                new Claim("Grandma.Says", "very nice boy"),
-            };
+		public IActionResult Authenticate()
+		{
+			var grandmaClaims = new List<Claim>
+			{
+				new Claim(ClaimTypes.Name, "Bob"),
+				new Claim(ClaimTypes.Email, "bob@fmail.com"),
+				new Claim("Grandma.Says", "very nice boy"),
+			};
 
-            var licenseClaims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Email, "bob@gmail.com"),
-                new Claim("DrivingLicense", "A+")
-            };
+			var licenseClaims = new List<Claim>
+			{
+				new Claim(ClaimTypes.Email, "bob@gmail.com"),
+				new Claim("DrivingLicense", "A+")
+			};
 
 
-            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
-            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government Identity");
+			var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+			var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government Identity");
 
-            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenseIdentity });
+			var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenseIdentity });
 
-            HttpContext.SignInAsync(userPrincipal);
-            // return View();
-            return RedirectToAction("Index");
-        }
+			HttpContext.SignInAsync(userPrincipal);
+			// return View();
+			return RedirectToAction("Index");
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Login(string username, string password)
-        {
-            var user = await _userManager.FindByNameAsync(username);
-            if (user != null)
-            {
-                var result = await _signinManager.PasswordSignInAsync(user, password, false, false);
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index");
-                }
-            }
-            return RedirectToAction("Index");
-        }
+		[HttpPost]
+		public async Task<IActionResult> Login(string username, string password)
+		{
+			var user = await _userManager.FindByNameAsync(username);
+			if (user != null)
+			{
+				var result = await _signinManager.PasswordSignInAsync(user, password, false, false);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index");
+				}
+			}
+			return RedirectToAction("Index");
+		}
 
-        public IActionResult Login() => View();
-        public IActionResult Register() => View();
+		public IActionResult Login() => View();
+		public IActionResult Register() => View();
 
-        [HttpPost]
-        public async Task<IActionResult> Register(string username, string password)
-        {
-            IdentityUser user = new IdentityUser
-            {
-                UserName = username,
-                Email = ""
-            };
-            var result = await _userManager.CreateAsync(user, password);
-            if (result.Succeeded)
-            {
-                var signinResult = await _signinManager.PasswordSignInAsync(user, password, false, false);
-                if (signinResult.Succeeded)
-                {
-                    return RedirectToAction("Index");
-                }
-            }
-            return RedirectToAction("Index");
-        }
+		[HttpPost]
+		public async Task<IActionResult> Register(string username, string password)
+		{
+			IdentityUser user = new IdentityUser
+			{
+				UserName = username,
+				Email = ""
+			};
+			var result = await _userManager.CreateAsync(user, password);
+			if (result.Succeeded)
+			{
+				// generation of the email token
+				var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+				var link = Url.Action(
+					nameof(VerifyEmail),
+					"Home",
+					new { userId = user.Id, code },
+					Request.Scheme,
+					Request.Host.ToString()
+				);
 
-        public async Task<IActionResult> Logout()
-        {
-            await _signinManager.SignOutAsync();
-            return RedirectToAction("Index");
-        }
-    }
+				var linkHtml = $"<a href='{link}'>Click To Verify</a>";
+				await _emailService.SendAsync("test@test.com", "verification", linkHtml, true);
+
+				return RedirectToAction("EmailVerification");
+			}
+			return RedirectToAction("Index");
+		}
+
+		public async Task<IActionResult> VerifyEmail(string userId, string code)
+		{
+			var user = await _userManager.FindByIdAsync(userId);
+			if (user == null)
+				return BadRequest();
+
+			var confirmResult = await _userManager.ConfirmEmailAsync(user, code);
+
+			if (confirmResult.Succeeded)
+			{
+				return View();
+			}
+
+			return BadRequest();
+		}
+
+		public IActionResult EmailVerification() => View();
+
+		public async Task<IActionResult> Logout()
+		{
+			await _signinManager.SignOutAsync();
+			return RedirectToAction("Index");
+		}
+	}
 }
--- IdentityExample/Startup.cs
+++ IdentityExample/Startup.cs
@@ -3,56 +3,71 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NETCore.MailKit.Extensions;
+using NETCore.MailKit.Infrastructure.Internal;
 
 namespace IdentityExample
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<AppDbContext>(config =>
-            {
-                config.UseInMemoryDatabase("Memory");
-            });
+	public class Startup
+	{
+		private readonly IConfiguration _config;
+		public Startup(IConfiguration config)
+		{
+			_config = config;
+		}
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<AppDbContext>(config =>
+			{
+				config.UseInMemoryDatabase("Memory");
+			});
 
-            services.AddIdentity<IdentityUser, IdentityRole>(config => {
-                config.Password.RequiredLength = 4;
-                config.Password.RequireDigit = false;
-                config.Password.RequireNonAlphanumeric = false;
-                config.Password.RequireUppercase = false;
-            })
-                .AddEntityFrameworkStores<AppDbContext>()
-                .AddDefaultTokenProviders();
-            
-            services.ConfigureApplicationCookie(config => {
-                config.Cookie.Name = "IdentityCookie";
-                config.LoginPath = "/Home/Login";
-            });
-            services.AddControllersWithViews();
-        }
+			services.AddIdentity<IdentityUser, IdentityRole>(config =>
+			{
+				config.Password.RequiredLength = 4;
+				config.Password.RequireDigit = false;
+				config.Password.RequireNonAlphanumeric = false;
+				config.Password.RequireUppercase = false;
+				config.SignIn.RequireConfirmedEmail = true;
+			})
+				.AddEntityFrameworkStores<AppDbContext>()
+				.AddDefaultTokenProviders();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            
-            app.UseRouting();
+			services.ConfigureApplicationCookie(config =>
+			{
+				config.Cookie.Name = "IdentityCookie";
+				config.LoginPath = "/Home/Login";
+			});
 
-            app.UseAuthentication(); // who are you?
-            app.UseAuthorization(); // are you allowed?
+			var mailKitOptions = _config.GetSection("Email").Get<MailKitOptions>();
+			services.AddMailKit(config => config.UseMailKit(mailKitOptions));
 
-            app.UseEndpoints(endpoints =>
-            {
-                // endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-                endpoints.MapDefaultControllerRoute();
-            });
-        }
-    }
+			services.AddControllersWithViews();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseRouting();
+
+			app.UseAuthentication(); // who are you?
+			app.UseAuthorization(); // are you allowed?
+
+			app.UseEndpoints(endpoints =>
+			{
+				// endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+				endpoints.MapDefaultControllerRoute();
+			});
+		}
+	}
 }
